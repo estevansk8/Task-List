f362208f351e950c92c71a914dcc3fcf151f316a
Estevan Sousa <estevanmartins03@gmail.com>
2024-01-11 10:19:24 -0300

    :construction:feat: Adding a TopAppBar

--- app/src/main/java/com/estevan/listadetarefas/view/ShowTasks.kt
+++ app/src/main/java/com/estevan/listadetarefas/view/ShowTasks.kt
@@ -1,10 +1,47 @@
 package com.estevan.listadetarefas.view
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.estevan.listadetarefas.ui.theme.Purple80
+import com.estevan.listadetarefas.ui.theme.White
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShowTasks(
     navController: NavController
 ){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+
+                title = { 
+                    Text(
+                        text = "Task List ",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = White
+                    )
+                }
+
+            )
+        }
+
+    ) {
+
+    }
 }
