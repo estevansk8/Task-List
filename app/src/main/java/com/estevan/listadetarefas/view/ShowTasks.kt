752a6826c046dc1caed90d7322d2560d8f13d799
Estevan Sousa <estevanmartins03@gmail.com>
2024-01-18 10:18:37 -0300

    :construction:feat: Adding button and making desing of ShowTasks

--- app/src/main/java/com/estevan/listadetarefas/view/ShowTasks.kt
+++ app/src/main/java/com/estevan/listadetarefas/view/ShowTasks.kt
@@ -1,17 +1,27 @@
 package com.estevan.listadetarefas.view
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.estevan.listadetarefas.R
+import com.estevan.listadetarefas.ui.theme.Black
+import com.estevan.listadetarefas.ui.theme.Purple40
 import com.estevan.listadetarefas.ui.theme.Purple80
 import com.estevan.listadetarefas.ui.theme.White
 
@@ -25,10 +35,8 @@ fun ShowTasks(
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
+                    containerColor = Purple40
                 ),
-
                 title = { 
                     Text(
                         text = "Task List ",
@@ -37,8 +45,21 @@ fun ShowTasks(
                         color = White
                     )
                 }
-
             )
+        },
+        containerColor = Black,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+
+                },
+                containerColor = Purple40,
+                shape = ShapeDefaults.ExtraLarge
+            ) {
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_add),
+                    contentDescription = "icon to save task")
+            }
         }
 
     ) {
